c5f2fdbc8ad264077f18286c7ce9580d59fa0a10
Pine Mizune <pinemz@gmail.com>
2019-11-01 13:01:00 +0900

    feat: add json properties

--- libs/slack/src/main/java/moe/pine/translatebot/slack/MessageEvent.java
+++ libs/slack/src/main/java/moe/pine/translatebot/slack/MessageEvent.java
@@ -1,15 +1,23 @@
 package moe.pine.translatebot.slack;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+/**
+ * @see <a href="https://github.com/nlopes/slack/blob/master/messages.go">slack/messages.go at master · nlopes/slack</a>
+ */
 @Data
 public class MessageEvent implements Event {
     public static final String TYPE = "message";
 
     private String type;
-    private String subtype;
     private String channel;
     private String user;
     private String text;
     private String ts;
+    @JsonProperty("thread_ts")
+    private String threadTs;
+    private String subtype;
+    private Boolean hidden;
+    private MessageEvent message;
 }
