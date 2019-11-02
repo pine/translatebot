61365f64e4a136b3fefae213625c9274377d9141
Pine Mizune <pinemz@gmail.com>
2019-11-03 02:16:50 +0900

    feat: add `MessageTextPreprocessor` [ci skip]

---
+++ app/src/main/java/moe/pine/translatebot/services/MessageTextPreprocessor.java
@@ -0,0 +1,10 @@
+package moe.pine.translatebot.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageTextPreprocessor {
+    public void execute() {
+
+    }
+}
