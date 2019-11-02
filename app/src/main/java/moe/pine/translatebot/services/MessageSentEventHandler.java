4f4b023ec7121e8b0bedaf92a780e04e925e1195
Pine Mizune <pinemz@gmail.com>
2019-11-03 03:22:41 +0900

    feat: implement TextPreprocessor

--- app/src/main/java/moe/pine/translatebot/services/MessageSentEventHandler.java
+++ app/src/main/java/moe/pine/translatebot/services/MessageSentEventHandler.java
@@ -12,6 +12,7 @@ import moe.translatebot.log.SentLog;
 import moe.translatebot.log.SentLogRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,6 +28,7 @@ public class MessageSentEventHandler {
     private final SlackClient slackClient;
     private final Translator translator;
     private final SentLogRepository sentLogRepository;
+    private final TextPreprocessor textPreprocessor;
 
     public void execute(final MessageEvent messageEvent) {
         final Matcher matcher = EMOTICON_ONLY_TEXT_PATTERN.matcher(messageEvent.getText());
@@ -35,32 +37,44 @@ public class MessageSentEventHandler {
         }
 
         final String text = messageEvent.getText();
-        translator.translate(text)
-            .ifPresent(translatedText -> {
-                log.info("Translated from \"{}\" to \"{}\"", text, translatedText);
+        final Optional<TextPreprocessor.Result> processedTextsOpt = textPreprocessor.execute(text);
+        if (processedTextsOpt.isEmpty()) {
+            return;
+        }
 
-                final String postingText = String.format(POSTING_TEXT_FORMAT, translatedText);
-                final boolean replyBroadcast =
-                    MessageEvent.Subtypes.THREAD_BROADCAST.equals(messageEvent.getSubtype());
-                final PostMessageRequest postMessageRequest =
-                    PostMessageRequest.builder()
-                        .username(slackProperties.getUsername())
-                        .threadTs(messageEvent.getThreadTs())
-                        .channel(messageEvent.getChannel())
-                        .text(postingText)
-                        .iconUrl(slackProperties.getIconUrl())
-                        .replyBroadcast(replyBroadcast)
-                        .build();
-                final PostMessageResponse postMessageResponse =
-                    slackClient.postMessage(postMessageRequest);
+        final TextPreprocessor.Result processedTexts = processedTextsOpt.get();
+        final Optional<String> translatedTextOpt = translator.translate(processedTexts.getText());
+        if (translatedTextOpt.isEmpty()) {
+            return;
+        }
 
-                final SentLog sentLog = SentLog.builder()
-                    .channel(messageEvent.getChannel())
-                    .sourceTs(messageEvent.getTs())
-                    .destinationTs(postMessageResponse.getTs())
-                    .build();
-                sentLogRepository.add(sentLog);
-            });
+        final String translatedText = translatedTextOpt.get();
+        log.info("Translated from \"{}\" to \"{}\"", processedTexts.getText(), translatedText);
 
+        final String postingText = String.format(POSTING_TEXT_FORMAT,
+            processedTexts.getPreText()
+                + translatedText
+                + processedTexts.getPostText());
+
+        final boolean replyBroadcast =
+            MessageEvent.Subtypes.THREAD_BROADCAST.equals(messageEvent.getSubtype());
+        final PostMessageRequest postMessageRequest =
+            PostMessageRequest.builder()
+                .username(slackProperties.getUsername())
+                .threadTs(messageEvent.getThreadTs())
+                .channel(messageEvent.getChannel())
+                .text(postingText)
+                .iconUrl(slackProperties.getIconUrl())
+                .replyBroadcast(replyBroadcast)
+                .build();
+        final PostMessageResponse postMessageResponse =
+            slackClient.postMessage(postMessageRequest);
+
+        final SentLog sentLog = SentLog.builder()
+            .channel(messageEvent.getChannel())
+            .sourceTs(messageEvent.getTs())
+            .destinationTs(postMessageResponse.getTs())
+            .build();
+        sentLogRepository.add(sentLog);
     }
 }
---
+++ app/src/main/java/moe/pine/translatebot/services/TextPreprocessor.java
@@ -0,0 +1,51 @@
+package moe.pine.translatebot.services;
+
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Slf4j
+@Component
+public class TextPreprocessor {
+    private static final Pattern PRE_TEXT_PATTERN =
+        Pattern.compile("^(?<preText>(?:\\s+|(?:<![a-z]+>)|(?:<@[A-Z0-9]+>)|(?::[\\w-+]+:))*)(?<behindText>.*)$");
+    private static final Pattern POST_TEXT_PATTERN =
+        Pattern.compile("^(?<centerText>.*?)(?<postText>(?:\\s+|(?:<![a-z]+>)|(?:<@[A-Z0-9]+>)|(?::[\\w-+]+:))*)$");
+
+    @Value
+    @Builder
+    public static class Result {
+        private String preText;
+        private String text;
+        private String postText;
+    }
+
+    public Optional<Result> execute(final String text) {
+        final Matcher preTextMatcher = PRE_TEXT_PATTERN.matcher(text);
+        if (!preTextMatcher.matches()) {
+            return Optional.empty();
+        }
+
+        final String preText = preTextMatcher.group("preText");
+        final String behindText = preTextMatcher.group("behindText");
+
+        final Matcher postTextMatcher = POST_TEXT_PATTERN.matcher(behindText);
+        if (!postTextMatcher.matches()) {
+            return Optional.empty();
+        }
+
+        final String centerText = postTextMatcher.group("centerText");
+        final String postText = postTextMatcher.group("postText");
+        return Optional.of(
+            Result.builder()
+                .preText(preText)
+                .text(centerText)
+                .postText(postText)
+                .build());
+    }
+}
