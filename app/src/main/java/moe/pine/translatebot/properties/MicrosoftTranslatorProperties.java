8c895bef3c5b2e45f2bf00b7e943fe2137c751ca
Pine Mizune <pinemz@gmail.com>
2019-11-12 02:31:52 +0900

    feat: add subscription key

--- app/src/main/java/moe/pine/translatebot/properties/MicrosoftTranslatorProperties.java
+++ app/src/main/java/moe/pine/translatebot/properties/MicrosoftTranslatorProperties.java
@@ -1,7 +1,12 @@
 package moe.pine.translatebot.properties;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import javax.validation.constraints.NotBlank;
+
+@Data
 @ConfigurationProperties("microsoft-translator")
 public class MicrosoftTranslatorProperties {
+    private @NotBlank String subscriptionKey;
 }
